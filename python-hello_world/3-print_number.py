d40efbedefe883ad21bd1c59a5e710e4eac74b9a
f-Ayuk <f.ayuk@alustudent.com>
2023-07-14 19:08:09 -0700

    python script to print integer stored in a variable

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-number in {98, 333, -98, 0, error}
-print(f"{number} Battery street")
+number= (98, 333, -98, 0, error)
+print(f"{number[0]} Battery street\n{number[1]} Battery street\n{number[2]} Battery street\n{number[3]} Battery street\n{number[4]} Battery street")
