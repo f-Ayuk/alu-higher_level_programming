81db445ba5c872bfc7dd5978c8b9c5ccf6b67674
f-Ayuk <f.ayuk@alustudent.com>
2023-07-24 08:34:08 -0700

    print the integer stored in the variable number

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-num1, num2, num3, num4, num5, street =98, 333, -98, 0, error, "Battery Street"
+num1, num2, num3, num4, num5, street =98, 333, -98, 0, "error", "Battery Street"
 print(f"{num1} {street}\n{num2} {street}\n{num3} {street}\n{num4} {street}\n{num5} {street}")
