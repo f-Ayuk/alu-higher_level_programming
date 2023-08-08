231e88076b64ab75d8b42070b648a79a696fdf7a
f-Ayuk <f.ayuk@alustudent.com>
2023-08-08 03:42:19 -0700

    printing a class Square that defines a square by: (based on 4-square.py)

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -9,6 +9,20 @@ class Square:
         """Iiinitialize the square with a private instance attribute size"""
         self.size = size
 
+    @property
+    def size(self):
+        """Return the current value of the private instance attribute size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the value of the private instance attribute size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
     def area(self):
         """Return the current square area"""
         return self.__size * self.__size
