714386e1736b35b96dea3229a3fe0bb01bb87c95
f-Ayuk <f.ayuk@alustudent.com>
2023-08-18 00:44:37 -0700

    printing a class Square that defines a square by: (based on 4-square.py)

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -28,11 +28,12 @@ class Square:
         return self.__size * self.__size
 
     def my_print(self):
-        """Print the square with the character #"""
+        """prints the square
+        Returns:
+            None
+        """
         if self.__size == 0:
-            print("")
-        else:
-            for i in range(self.__size):
-                for j in range(self.__size):
-                    print("#", end="")
-                print("")
+            print()
+            return
+        for i in range(self.__size):
+            print("".join(["#" for j in range(self.__size)]))
