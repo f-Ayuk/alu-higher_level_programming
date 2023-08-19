6fe8212c468f945ef9ebac6a5f15a8fa753df36c
f-Ayuk <f.ayuk@alustudent.com>
2023-08-19 15:03:23 -0700

    function for dividing matrices

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -6,12 +6,10 @@ def matrix_divided(matrix, div):
     """divides matrix by scalar integer, rounded to two decimal places"""
     import decimal
     error_msg = "matrix must be a matrix (list of lists) of integers/floats"
-    
     if type(matrix) is not list:
         raise TypeError(error_msg)
     len_rows = []
     row_count = 0
-    
     for row in matrix:
         if type(row) is not list:
             raise TypeError(error_msg)
@@ -20,19 +18,14 @@ def matrix_divided(matrix, div):
             if type(element) not in [int, float]:
                 raise TypeError(error_msg)
         row_count += 1
-    
     if len(set(len_rows)) > 1:
         raise TypeError("Each row of the matrix must have the same size")
-    
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
-    
     if int(div) == 0:
         raise ZeroDivisionError("division by zero")
-
     new_matrix = []
     for row in matrix:
         new_row = [round(element / div, 2) for element in row]
         new_matrix.append(new_row)
-
     return new_matrix
