41cae63bcad0c0df262324bc093573ba3ba424eb
f-Ayuk <f.ayuk@alustudent.com>
2023-08-19 15:16:39 -0700

    function printing a square with a special character

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Medule printing a square using a special character"""
+
+
+def print_square(size):
+    """prints a square with "#"'s that has a length of size """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size > 0:
+        print(("#" * size + "\n") * size, end="")
