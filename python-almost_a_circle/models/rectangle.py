8bc25ca2d9037867fadf03ef75e6ba7f22a0eea8
f-Ayuk <f.ayuk@alustudent.com>
2023-08-20 18:23:31 -0700

    removing space

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -143,7 +143,7 @@ class Rectangle(Base):
     def __str__(self):
         """Return the print() and str() representation of the Rectangle."""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                                                        self.x, 
-                                                        self.y, 
-                                                        self.width, 
+                                                        self.x,
+                                                        self.y,
+                                                        self.width,
                                                         self.height)
