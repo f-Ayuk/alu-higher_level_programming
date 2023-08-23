3862f199f19ab3653ac83ea3ae36d51ab0f13bbe
f-Ayuk <f.ayuk@alustudent.com>
2023-08-23 02:55:13 -0700

    python class based on a sorting algorithm

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/ussr/bin/python3
+""""sorting algorithm"""
+
+
+class MyList(list):
+    """A class that inherits from list"""
+
+    def print_sorted(self):
+        """Prints the list, but sorted (ascending sort)"""
+        print(sorted(self)) # sorted() is a built-in function that returns a new sorted list
