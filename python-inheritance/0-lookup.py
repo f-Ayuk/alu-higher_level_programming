33263eaadb7c9435373d9d6ffd80f52acbb37974
f-Ayuk <f.ayuk@alustudent.com>
2023-08-23 02:50:49 -0700

    lookup function

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,6 +3,6 @@
 
 
 def lookup(obj):
-    """dir(), a built-in function which returns list of names in the object's namespace"""
+    """a built-in function to return list of names in the object's namespace"""
 
     return dir(obj)
