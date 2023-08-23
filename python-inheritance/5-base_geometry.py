0bdd629eb0ae3ddcba3a5927d839bf5bf592904a
f-Ayuk <f.ayuk@alustudent.com>
2023-08-23 11:05:10 -0700

    define a class for BaseGeometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""pass is a keyword that does nothing"""
+
+
+class BaseGeometry:
+    """An empty class"""
+    pass  # used as a placeholder for empty classes, functions, loops, etc.
