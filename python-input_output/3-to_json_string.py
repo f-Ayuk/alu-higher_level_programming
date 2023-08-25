2cb4fe36d11a3c3a655c2d55c41b64dd37fc94eb
f-Ayuk <f.ayuk@alustudent.com>
2023-08-25 07:46:54 -0700

    the JSON representation of an object

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""function that returns the JSON representation of an object (string)"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """A function that returns the JSON representation of an object (string).
+    Args:
+        my_obj (object): The object to be serialized.
+    Returns:
+        str: The JSON representation of the object.
+    """
+    return json.dumps(my_obj)
