e0ef5b8136da5bb8609c1b10da22e2a3f0adb5d6
f-Ayuk <f.ayuk@alustudent.com>
2023-08-25 04:03:25 -0700

    updates

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -2,6 +2,21 @@
 """Rectangle based on 7-base_geometry"""
 
 
+class BaseGeometry:
+    """A class that represents a base geometry."""
+
+    def area(self):
+        """A method that raises an exception for unimplemented area."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """A method that validates an integer value."""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        value = int(value)
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
 class Rectangle(BaseGeometry):
     """A class that represents a rectangle geometry."""
 
