5080696b078e29994e45b203188432bb40110f2b
f-Ayuk <f.ayuk@alustudent.com>
2023-08-25 05:24:54 -0700

    updates

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -17,6 +17,7 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
+
 class Rectangle(BaseGeometry):
     """A class that represents a rectangle geometry."""
 
