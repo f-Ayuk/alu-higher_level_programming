b4940adca89a68f99cd20a138f81a7ade0613a82
f-Ayuk <f.ayuk@alustudent.com>
2023-08-25 07:05:20 -0700

    function to read files

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""function to read text files"""
+
+
+def read_file(filename=""):
+    """A function that reads a text file and prints it to stdout.
+
+    Args:
+        filename (str): The name of the file to be read. Defaults to "".
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
