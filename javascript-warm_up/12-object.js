54c0fa58222d7affde793875c6942ae4c535d871
f-Ayuk <f.ayuk@alustudent.com>
2023-10-01 12:25:44 -0700

    addition function that returns a value

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -6,6 +6,6 @@ const myObject = {
 };
 console.log(myObject);
 
-myObject.value = 89;// changing value from 12 to 89 
+myObject.value = 89; // changing value from 12 to 89 
 
 console.log(myObject);
