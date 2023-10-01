6b6092feb1335d7b14aa7fcb8f783f26b9a80419
f-Ayuk <f.ayuk@alustudent.com>
2023-10-01 02:10:15 -0700

    Printing 3 different lines

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+console.log('C is fun'); // print the first line
+console.log('Python is cool'); // print the second line
+console.log('JavaScript is amazing'); // print the third line
