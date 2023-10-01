3275951569b43435ef9ee6e197df4e2cf02b2275
f-Ayuk <f.ayuk@alustudent.com>
2023-10-01 03:04:44 -0700

    passing arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
-let args = process.argv.slice(2); // get the array of arguments passed to the script, excluding the first two elements (node and file name)
+const args = process.argv.slice(2); // get the array of arguments passed to the script, excluding the first two elements (node and file name)
 if (args.length === 0) { // check if the array is empty
   console.log('No argument'); // print "No argument" if no arguments are passed
-} 
-  else if (args.length === 1) { // check if the array has only one element
+} else if (args.length === 1) { // check if the array has only one element
   console.log('Argument found'); // print "Argument found" if only one argument is passed
-}
-    else { // otherwise
-    console.log('Arguments found'); // print "Arguments found" if more than one argument is passed
+} else { // otherwise
+  console.log('Arguments found'); // print "Arguments found" if more than one argument is passed
 }
