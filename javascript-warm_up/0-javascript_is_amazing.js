db3dc81c0581b967e16542b52b84f3fb3cd79701
f-Ayuk <f.ayuk@alustudent.com>
2023-10-01 02:05:35 -0700

    script that prints Javascript is amazing

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
-const myVar = "JavaScript is amazing"; // declare and initialize a constant variable called myVar
+const myVar = 'JavaScript is amazing'; // declare and initialize a constant variable called myVar
 console.log(myVar); // print the value of myVar to the console
