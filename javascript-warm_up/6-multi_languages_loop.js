53ac47ba6cf15a841950c2f73ed4ddf3e6596fef
f-Ayuk <f.ayuk@alustudent.com>
2023-10-01 02:57:02 -0700

    using an array of string and loop in javascript

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let languages = ['C is fun', 'Python is cool', 'JavaScript is amazing']; // create an array of strings with the three lines
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing']; // create an array of strings with the three lines
 for (let i = 0; i < languages.length; i++) { // use a for loop to iterate over the array
   console.log(languages[i]); // print each element of the array using console.log
 }
