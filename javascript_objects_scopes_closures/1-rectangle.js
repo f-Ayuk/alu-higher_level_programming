50fecfa04aaf70e64c2416eb86f0fc5770b76d88
f-Ayuk <f.ayuk@alustudent.com>
2023-10-01 13:10:19 -0700

    initialising the first two attributes

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -3,7 +3,7 @@
 // Define the class Rectangle
 class Rectangle {
     // Define the constructor with two parameters w and h
-    constructor(w, h) {
+    constructor (w, h) {
         // Initialize the instance attribute width with the value of w
         this.width = w;
         // Initialize the instance attribute height with the value of h
