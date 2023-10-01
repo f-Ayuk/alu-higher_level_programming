88e7983564e58ad14ce3ec72482fb7fd79d5ef15
f-Ayuk <f.ayuk@alustudent.com>
2023-10-01 11:23:10 -0700

    swapping in javascript

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,10 +4,10 @@ const args = process.argv.slice(2); // get the array of arguments passed to the 
 if (args.length === 0 || args.length === 1) { // check if the array is empty or has only one element
   console.log(0); // print 0 if no argument or only one argument is passed
 } else { // otherwise
-  let nums = args.map(arg => parseInt(arg)); // map the array of arguments to an array of integers using the parseInt function
-  let max = Math.max(...nums); // find the maximum value in the array using the Math.max function with the spread operator
+  const nums = args.map(arg => parseInt(arg)); // map the array of arguments to an array of integers using the parseInt function
+  const max = Math.max(...nums); // find the maximum value in the array using the Math.max function with the spread operator
   let secondMax = -Infinity; // initialize a variable to store the second maximum value, starting from -Infinity
-  for (let num of nums) { // use a for-of loop to iterate over the array of integers
+  for (const num of nums) { // use a for-of loop to iterate over the array of integers
     if (num < max && num > secondMax) { // check if the current element is less than the maximum value and greater than the second maximum value
       secondMax = num; // update the second maximum value with the current element
     }
