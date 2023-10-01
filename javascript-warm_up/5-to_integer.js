eb3a6022aa732990233908fbe515daeafd879bdb
f-Ayuk <f.ayuk@alustudent.com>
2023-10-01 03:15:21 -0700

    conversion of arguments to numbers

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const arg = process.argv[2]; // get the first argument passed to the script, which is the third element in the process.argv array
 const num = parseInt(arg); // try to convert the argument to an integer using the parseInt function
 if (isNaN(num)) { // check if the result is NaN, which means the argument can't be converted to an integer
-  console.log("Not a number"); // print "Not a number" if the argument can't be converted
+  console.log('Not a number'); // print "Not a number" if the argument can't be converted
 } else { // otherwise
-  console.log("My number: " + num); // print "My number: " followed by the converted integer
+  console.log('My number: ' + num); // print "My number: " followed by the converted integer
 }
