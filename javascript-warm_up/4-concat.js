e09e683e6c77385d593bde309276df7d32667651
f-Ayuk <f.ayuk@alustudent.com>
2023-10-01 02:43:21 -0700

    concatenation in javascript

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+let arg1 = process.argv[2]; // get the first argument passed to the script, which is the third element in the process.argv array
+let arg2 = process.argv[3]; // get the second argument passed to the script, which is the fourth element in the process.argv array
+console.log(arg1 + ' is ' + arg2); // print the two arguments concatenated with " is " in between
