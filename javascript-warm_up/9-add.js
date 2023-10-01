52ce0379cacf29f7f76f4537ee8b0c7b5711af81
f-Ayuk <f.ayuk@alustudent.com>
2023-10-01 10:34:34 -0700

    addition of two integers

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -6,5 +6,5 @@ function add(a, b) { // define a function with the prototype function add(a, b)
 
 const arg1 = parseInt(process.argv[2]); // get the first argument passed to the script and try to convert it to an integer
 const arg2 = parseInt(process.argv[3]); // get the second argument passed to the script and try to convert it to an integer
-const result = add(arg1, arg2); // call the add function with the two arguments and store the result
+let result = add(arg1, arg2); // call the add function with the two arguments and store the result
 console.log(result); // print the result using console.log
