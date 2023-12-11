2f6876287d6df8e2e56a22107959a697a8de0c82
f-Ayuk <f.ayuk@alustudent.com>
2023-12-11 03:11:39 -0800

    a class Student that defines a student by: (based on 9-student.py)

---
+++ python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Defines class student'''
+
+
+class Student:
+    '''Represents user data of a student'''
+    def __init__(self, first_name, last_name, age):
+        '''Initializes a new student inatsnce'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        '''Returns a dictionary representation of the student instance'''
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
