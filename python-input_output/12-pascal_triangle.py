fda25edf1075be56d76792fad08e2ca1223e6043
f-Ayuk <f.ayuk@alustudent.com>
2023-12-11 05:20:13 -0800

    Pascal triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -7,7 +7,18 @@ def pascal_triangle(n):
     '''Function returns a list of lists of integers
     representing a pascals triangle
     '''
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
+    if n <= 0:
+        return []
 
-print_triangle(pascal_triangle(5))
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
