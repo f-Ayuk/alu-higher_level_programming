058d12b027f07bf2c0961bf0c628ee7be734a18f
f-Ayuk <f.ayuk@alustudent.com>
2023-12-11 03:16:38 -0800

    a class Student that defines a student by: (based on 9-student.py)

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -10,9 +10,10 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         '''Returns a dictionary representation of the student instance'''
         if attrs is None:
             return self.__dict__
         else:
-            return {key: value for key, value in self.__dict__.items() if key in attrs}
+            return 
+                    {key: value for key, value in self.__dict__.items() if key in attrs}
