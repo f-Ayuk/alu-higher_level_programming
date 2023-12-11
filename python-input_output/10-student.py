d8fb1829e5258f649966f0fb8cef153bd70987e5
f-Ayuk <f.ayuk@alustudent.com>
2023-12-11 04:35:50 -0800

    Student to JSON with filter

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -19,5 +19,8 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            return 
-            {key: value for key, value in self.__dict__.items() if key in attrs}
+            new_dict = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    new_dict[key] = value
+            return new_dict
