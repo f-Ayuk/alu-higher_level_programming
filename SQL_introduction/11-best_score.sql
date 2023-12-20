05186a5eb2d9ddc342eb10a1e48941d699ae4180
f-Ayuk <f.ayuk@alustudent.com>
2023-12-20 02:50:01 -0800

    Option to select the best

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,12 @@
+-- Select score and name columns from second_table
+
+SELECT score, name
+FROM second_table
+
+-- Filter by score >= 10
+
+WHERE score >= 10
+
+-- Order by score in descending order
+
+ORDER BY score DESC;
