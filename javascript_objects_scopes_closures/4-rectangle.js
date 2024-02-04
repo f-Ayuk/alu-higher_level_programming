5bc75566b00978e9b33612962113b65dd8a99d64
f-Ayuk <f.ayuk@alustudent.com>
2024-02-04 13:40:45 -0800

    updating the class Rectangle

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,24 +1,32 @@
 #!/usr/bin/node
-
 class Rectangle {
-    constructor(w, h) {
-	    this.width = w;
-	    this.height = h;
-	    if (w <= 0 || h <= 0) {
-            this.width = 0;
-            this.height = 0;
-	    }
-	    print(){
-		    for (let i =0; i < this.height; i++) {
-			    console.log('X'.repeat(this.width));
-		    }
-	    rotate() {
-		    [this.width, this.height] = [this.height, this.width];
-	    }
-	    double() {
-		    this.width *= 2;
-		    this.height *= 2;
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
+    }
+  }
+
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 
 module.exports = Rectangle;
