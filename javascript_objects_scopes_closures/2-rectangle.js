763aa35966aefe086514d068be9f858e4dc660df
f-Ayuk <f.ayuk@alustudent.com>
2024-02-04 13:43:12 -0800

    updating the class Rectangle

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
-// Define the class Rectangle
 class Rectangle {
-  // Define the constructore with two parameters w and h
   constructor (w, h) {
-    // check if w and h are positive integers
-    if (w > 0 && h > 0) {
-      // initialise the instance
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
-    } else {
-      return {};
     }
   }
 }
