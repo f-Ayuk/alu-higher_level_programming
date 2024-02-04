bbf830fc302b787a80404f3382938082df2a5a4a
f-Ayuk <f.ayuk@alustudent.com>
2024-02-04 13:30:41 -0800

    executing 2-rectangle.js

--- javascript_objects_scopes_closures/2-main.js
+++ javascript_objects_scopes_closures/2-main.js
@@ -2,26 +2,26 @@
 const Rectangle = require('./2-rectangle');
 
 const r1 = new Rectangle(3, 3);
-console.log('Rectangle 'r1);
+console.log('Rectangle '); console.log(r1);
 console.log(r1.width);
 console.log(r1.height);
 
 const r2 = new Rectangle(3, -3);
-console.log('Rectangle 'r2);
+console.log('Rectangle '); console.log(r2);
 console.log(r2.width);
 console.log(r2.height);
 
 const r3 = new Rectangle(3);
-console.log('Rectangle 'r3);
+console.log('Rectangle '); console.log(r3);
 console.log(r3.width);
 console.log(r3.height);
 
 const r4 = new Rectangle(0, 3);
-console.log('Rectangle 'r4);
+console.log('Rectangle '); console.log(r4);
 console.log(r4.width);
 console.log(r4.height);
 
-const r5 = new Rectangle(3, 3);
-console.log('Rectangle 'r5);
+const r5 = new Rectangle();
+console.log('Rectangle '); console.log(r5);
 console.log(r5.width);
 console.log(r5.height);
