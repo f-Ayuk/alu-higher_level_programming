b7803118f736e2dc40f48e71c4018c8403b70926
f-Ayuk <f.ayuk@alustudent.com>
2024-02-04 10:34:17 -0800

    printing a square

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const arg = process.argv[2]; 
+/*const arg = process.argv[2]; 
 if (isNaN(arg)) { // check if the result is NaN, which means the argument can't be converted to an integer
   console.log('Missing size'); // print "Missing size" if the argument can't be converted
 } else { // otherwise
@@ -12,4 +12,27 @@ if (isNaN(arg)) { // check if the result is NaN, which means the argument can't 
       console.log(square); // print the square string using console.log
       }
   }
+}*/
+
+// Get the first argument as a number
+let size = Number(process.argv[2]);
+// Check if the argument is a valid positive integer
+if (isNaN(size) || size <= 0) {
+  // Print "Missing size"
+  console.log("Missing size");
+} else {
+  // Initialize an empty string to store the output
+  let output = "";
+  // Loop from 0 to size - 1
+  for (let i = 0; i < size; i++) {
+    // Loop from 0 to size - 1
+    for (let j = 0; j < size; j++) {
+      // Append the character X to the output
+      output += "X";
+    }
+    // Append a newline character to the output
+    output += "\n";
+  }
+  // Print the output
+  console.log(output);
 }
