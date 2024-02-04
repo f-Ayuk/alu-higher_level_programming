a6d66ffc105a2b1751ad7e76bb9c59f662de381f
f-Ayuk <f.ayuk@alustudent.com>
2024-02-04 13:59:36 -0800

    updating class Rectangle

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,23 +1,10 @@
 #!/usr/bin/node
-
 const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
- constructor(size) {
+  constructor (size) {
     super(size, size);
-    this.size = size;
- }
-
- double() {
-    this.size *= 2;
-    super.double();
- }
-
- print() {
-    for (let i = 0; i < this.size; i++) {
-      console.log('X'.repeat(this.size));
-    }
- }
+  }
 }
 
 module.exports = Square;
