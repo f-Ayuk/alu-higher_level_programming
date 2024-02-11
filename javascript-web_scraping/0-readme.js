bb29aa1b462a2b8f814b2570122007319fbb5f37
f-Ayuk <f.ayuk@alustudent.com>
2024-02-11 07:50:24 -0800

    script reads and prints the content of a file

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+// Import the file system module
+const fs = require('fs');
+
+// Get the file path from the first argument
+const filePath = process.argv[2];
+
+// Read the file in utf-8 encoding
+fs.readFile(filePath, 'utf8', (err, data) => {
+  // If an error occurred, print the error object
+  if (err) {
+    console.error(err);
+  } else {
+    // Otherwise, print the file content
+    console.log(data);
+  }
+});
