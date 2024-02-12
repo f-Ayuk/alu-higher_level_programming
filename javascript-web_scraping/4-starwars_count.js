0b464fae2f7193a4c50ae995b1c5131955973afb
f-Ayuk <f.ayuk@alustudent.com>
2024-02-12 00:55:35 -0800

    script that prints the number of movies where a character is present

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -12,36 +12,34 @@ const wedgeId = 18;
 // Make a GET request to the API
 request(apiUrl, (error, response, body) => {
 // If an error occurred, handle it
-  if (err) {
-    console.error(err);
-  } 
-  // If the request was successful, count the movies
-  if (response.statusCode === 200) {
-    // Parse the JSON data from the body
-    const data = JSON.parse(body);
+  if (!err) { 
+    // If the request was successful, count the movies
+    if (response.statusCode === 200) {
+      // Parse the JSON data from the body
+      const data = JSON.parse(body);
 
-    // Get the array of movies from the data
-    const movies = data.results;
+      // Get the array of movies from the data
+      const movies = data.results;
 
-    // Initialize a counter for the movies with Wedge Antilles
-    let count = 0;
+      // Initialize a counter for the movies with Wedge Antilles
+      let count = 0;
 
-    // Loop through the movies
-    for (const movie of movies) {
-      // Get the array of characters from the movie
-      const characters = movie.characters;
+      // Loop through the movies
+      for (const movie of movies) {
+        // Get the array of characters from the movie
+        const characters = movie.characters;
 
-      // Check if Wedge Antilles is in the characters array
-      if (characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeId}/`)) {
-        // Increment the counter
-        count++;
-      }
+        // Check if Wedge Antilles is in the characters array
+        if (characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeId}/`)) {
+          // Increment the counter
+          count++;
+        } 
+      } 
+
+      // Print the number of movies with Wedge Antilles
+      console.log(count);
+    } else {
+      // Otherwise, print an error message
+      console.error(`Error: ${response.statusCode}`);
     }
-
-    // Print the number of movies with Wedge Antilles
-    console.log(count);
-  } else {
-    // Otherwise, print an error message
-    console.error(`Error: ${response.statusCode}`);
-  }
-});
+  });
