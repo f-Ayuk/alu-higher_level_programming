9bbfb1c2f676fb15454fa3ef291205eb1ed47647
f-Ayuk <f.ayuk@alustudent.com>
2024-02-12 00:38:39 -0800

    script that gets the contents of a webpage and stores it in a file

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Import the request and fs modules
+const request = require('request');
+const fs = require('fs');
+
+// Get the URL and the file path from the arguments
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+// Make a GET request to the URL and pipe the response to the file
+request(url).pipe(fs.createWriteStream(filePath, {encoding: 'utf8'}));
